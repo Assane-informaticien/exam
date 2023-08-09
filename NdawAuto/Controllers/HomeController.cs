7b75a9c2386fa30f9ff6af0515fac0fb87f73261
unknown <DELL@DESKTOP-06EQC2F.(none)>
2023-08-09 18:29:30 +0000

    Premier commit

---
+++ NdawAuto/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NdawAuto.Models;
+using System.Diagnostics;
+
+namespace NdawAuto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Gamme()
+        {
+            return View();
+        }
+
+        public IActionResult Offres()
+        {
+            return View();
+        }
+
+        public IActionResult APropos()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NdawAuto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NdawAuto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
